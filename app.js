1c43ac5e69a6d07e5c7e2820d836f75fb4c8382b
filleAUa81 <2401340071sg@cyber-u.ac.jp>
2026-05-16 15:14:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
-var catRouter = require('./routes/cat');
-var notes_from_bRouter = require('./routes/notes_from_b');
+//var catRouter = require('./routes/cat');
+//var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
@@ -18,8 +18,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
-app.use('/cat', catRouter);
-app.use('/notes_from_b', notes_from_bRouter);
+//app.use('/cat', catRouter);
+//app.use('/notes_from_b', notes_from_bRouter);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
