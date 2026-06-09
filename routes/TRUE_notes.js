72d3706094d858b4c5a89891586d7d812e6814cd
Fumi <fumi@bunyaitonoMacBook-Pro.local>
2026-06-09 09:38:13 +0900

    Add mongodb access

--- routes/TRUE_notes.js
+++ routes/TRUE_notes.js
@@ -1,19 +1,15 @@
-const _crypto = require('crypto');
-globalThis.crypto = _crypto;
-globalThis.crypto.getRandomValues = (arr) => _crypto.randomFillSync(arr);
-
 var express = require('express');
 var router = express.Router();
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "****";
+const uri ="mongodb+srv://cu2401340071:b7cgH5u_G@cluster0.y5uxhta.mongodb.net/food?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
 const database = client.db('notes');
 const notes = database.collection('notes');
 // idが１のドキュメントを取得
-const query = {};
+const query = { id: 2 };
 const note = await notes.findOne(query);
 res.json(note);
 })
